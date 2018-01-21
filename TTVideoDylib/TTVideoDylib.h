a55842e906c3149f9094f1a8c5d836bfe79a42a5
huyong <erpapa@huyongdeair.lan>
2018-01-21 13:46:40 +0800

    Initial Commit

---
+++ TTVideoDylib/TTVideoDylib.h
@@ -0,0 +1,13 @@
+//  weibo: http://weibo.com/xiaoqing28
+//  blog:  http://www.alonemonkey.com
+//
+//  TTVideoDylib.h
+//  TTVideoDylib
+//
+//  Created by huyong on 2018/1/21.
+//  Copyright (c) 2018年 erpapa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
